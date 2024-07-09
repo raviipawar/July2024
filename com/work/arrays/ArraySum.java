9e44d7c217ce2f2b309583fccf605746f936c4e9
raviipawar <ravibalajipawar@gmail.com>
2024-07-09 18:06:49 +0530

    Latest Commit

---
+++ com/work/arrays/ArraySum.java
@@ -0,0 +1,11 @@
+package com.work.arrays;
+
+import java.util.Arrays;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int sum = Arrays.stream(arr).sum();
+        System.out.println("Sum of Array is :"+sum);
+    }
+}
